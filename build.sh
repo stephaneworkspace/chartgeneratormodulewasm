876cad1fe60b9ee6e6e77215da750d0fdb3a328a
stephaneworkspace <s.bressani@bluewin.ch>
2020-09-12 01:27:10 +0200

    wasm rust + react ok

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ yarn install
 rm -rf dist
 rm -rf pkg
 #yarn build
-wasm-pack build
+wasm-pack build --release --out-name index
+#--target nodejs
+# https://github.com/rustwasm/wasm-pack/issues/199 not fixed
+#wasm-pack pack
+#wasm-pack publish
 #yarn serve
