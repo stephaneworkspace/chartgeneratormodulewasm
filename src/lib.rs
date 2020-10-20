c2a71a6f75876ac5d9b95d95cdb3d46c137ab2d3
stephaneworkspace <s.bressani@bluewin.ch>
2020-10-20 16:34:58 +0200

    new fn for print svg of scale

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 extern crate base64;
 use base64::encode;
 use chartgeneratorsvg::{
-    chord_list_wasm, scale_list_wasm, StructChart, TraitDraw,
+    chord_list_wasm, scale_list_wasm, scale_print_wasm, StructChart, TraitDraw,
 };
 use wasm_bindgen::prelude::*;
 
@@ -42,4 +42,10 @@ pub fn chord_list() -> String {
 #[wasm_bindgen]
 pub fn scale_list_select() -> String {
     scale_list_wasm().into()
+}
+
+#[wasm_bindgen]
+pub fn scale_svg(scale: &str, note: &str) -> String {
+    let scale = scale_print_wasm(scale, note);
+    scale.into()
 }
