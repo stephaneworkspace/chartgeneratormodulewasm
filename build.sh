06cb8b20001db98cab82e9b48240caed6bf397a5
stephaneworkspace <s.bressani@bluewin.ch>
2020-10-21 18:36:18 +0200

    small fix out of grid

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+echo "don't forgot to change version in package.json before... or do it later with the same that Cargo.toml"
+cp ./pkg/package.json ./tmp.json
 cargo update
 yarn install
 rm -rf dist
 rm -rf pkg
 #yarn build
 wasm-pack build --release --out-name index
+mv ./tmp.json ./pkg/package.json
 #--target nodejs
 # https://github.com/rustwasm/wasm-pack/issues/199 not fixed
 #wasm-pack pack
