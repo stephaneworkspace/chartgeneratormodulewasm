22f70aa40d95b8f26d51f41ce6cac0f69b505b03
stephaneworkspace <s.bressani@bluewin.ch>
2020-10-24 01:56:10 +0200

    Add a Result<> structure... need to do a Promise in frontend react

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@ use chartgeneratorsvg::interface::{InterfaceWasm, TraitChord, TraitScale};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct UkuleleWasm {
-    theme: String, // TODO better later
-}
+pub struct UkuleleWasm {}
 
 #[wasm_bindgen]
 extern "C" {
@@ -22,30 +20,36 @@ extern "C" {
 pub fn greet(name: &str) {
     log("OK !");
     alert(&format!("Hello, {}!", name));
+//.into()
 }*/
-#[wasm_bindgen(catch)]
+#[wasm_bindgen]
 impl UkuleleWasm {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        Self {
-            theme: "dark".to_string(), // TODO better later
-        }
+        Self {}
     }
 
-    pub fn chord_list_multiple(
-        &self,
-        note: &str,
-        fret_position: FretID,
-    ) -> String {
-        InterfaceWasm::chord_list(note, fret_position).into()
+    /// Get a json in Vec<DtoNote> of chord list with multiple svg
+    /// More info about the Dto:
+    /// use chartgeneratorsvg::generate::{DtoChord, DtoNote};
+    ///
+    /// # Arguments
+    ///
+    /// * `note` - Valid note (parsed after in Note::from_str) or empty/invalid
+    /// for select all note
+    /// * `fret_position` - Fret position
+    ///
+    ///
+    pub fn chord_list(&self, note: &str, fret_position: FretID) -> String {
+        InterfaceWasm::chord_list(note, fret_position)
     }
 
     pub fn scale_list_select(&self) -> String {
-        InterfaceWasm::scale_list_wasm().into()
+        InterfaceWasm::scale_list_wasm()
     }
 
     #[wasm_bindgen(catch)]
-    pub fn scale_unique_svg(
+    pub fn scale_svg_unique(
         &self,
         scale_short: &str,
         tonic: &str,
@@ -55,13 +59,18 @@ impl UkuleleWasm {
             Err(e) => Err(JsValue::from_str(e.name.as_str())),
         }
     }
-    pub fn scale_chord_list_multiple(
+
+    #[wasm_bindgen(catch)]
+    pub fn scale_chord_list(
         &self,
         scale_short: &str,
         tonic: &str,
         fret_position: FretID,
-    ) -> String {
-        InterfaceWasm::scale_chord_list(scale_short, tonic, fret_position)
-            .into()
+    ) -> Result<String, JsValue> {
+        match InterfaceWasm::scale_chord_list(scale_short, tonic, fret_position)
+        {
+            Ok(res) => Ok(res),
+            Err(e) => Err(JsValue::from_str(e.name.as_str())),
+        }
     }
 }
