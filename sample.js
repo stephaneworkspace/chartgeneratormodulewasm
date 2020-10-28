47c807f95d1f2982229af7ff08e14717324e66c6
stephaneworkspace <s.bressani@bluewin.ch>
2020-10-28 20:30:01 +0100

    change change but no debuger...

--- sample.js
+++ sample.js
@@ -4,6 +4,11 @@ const fs = require('fs');
 export function read_file() {
 		console.log('2');
 		console.log(path.resolve(__dirname));
+		try {
+			console.log(fs.readFileSync(path.resolve(__dirname, 'test.txt'), null).toString());
+		} catch (err) {
+			console.log(err);
+		}
 		return fs.readFileSync(path.resolve(__dirname, 'test.txt'), null).toString(); // for test
     //return fs.readFileSync(path.resolve(__dirname, 'test.txt'), null).buffer;
 }
