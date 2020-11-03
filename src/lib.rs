59a1564a2b82ee3fa4e158acf21e0a4a47f4158b
stephaneworkspace <s.bressani@bluewin.ch>
2020-11-03 18:24:07 +0100

    impl light/dark

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
 use chartgeneratorsvg::chord::FretID;
-use chartgeneratorsvg::interface::{InterfaceWasm, TraitChord, TraitScale};
+use chartgeneratorsvg::interface::{
+    InterfaceWasm2 as InterfaceWasm, InterfaceWasmChord, InterfaceWasmScale,
+};
+use chartgeneratorsvg::svg_draw::Theme;
 use std::str::FromStr;
 use ukulele_midi::SoundBytes;
 use ukulele_midi::Variant;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct UkuleleWasm {}
+pub struct UkuleleWasm {
+    _theme: Theme,
+    interface: InterfaceWasm,
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -29,8 +35,10 @@ pub fn greet(name: &str) {
 #[wasm_bindgen]
 impl UkuleleWasm {
     #[wasm_bindgen(constructor)]
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(theme: &str) -> Self {
+        let _theme = Theme::from_str(theme).unwrap_or(Theme::Light);
+        let interface = InterfaceWasm::new(_theme.clone());
+        Self { _theme, interface }
     }
 
     /// Get a json in Vec<DtoNote> of chord list with multiple svg
@@ -50,16 +58,7 @@ impl UkuleleWasm {
         chord: &str,
         fret_position: FretID,
     ) -> String {
-        InterfaceWasm::chord_list(note, chord, fret_position)
-    }
-
-    pub fn chord_list_experimental(
-        &self,
-        note: &str,
-        chord: &str,
-        fret_position: FretID,
-    ) -> String {
-        InterfaceWasm::chord_list_experimental(note, chord, fret_position)
+        self.interface.chord_list(note, chord, fret_position)
     }
 
     /// Get a Uint8Array (Javascript/Typescript) with the wav inside
@@ -101,22 +100,38 @@ impl UkuleleWasm {
         }
     }
 
+    /// List of scale type
     pub fn scale_list_select(&self) -> String {
-        InterfaceWasm::scale_list_wasm()
+        self.interface.scale_list_wasm()
     }
 
+    /// Generate svg for a scale
+    ///
+    /// # Arguments
+    ///
+    /// * `scale_short` - // TODO for complete the documentation check out in
+    /// file in other rust project
+    /// * `tonic` - Valid note (parsed after in Note::from_str) or Catch
     #[wasm_bindgen(catch)]
     pub fn scale_svg_unique(
         &self,
         scale_short: &str,
         tonic: &str,
     ) -> Result<String, JsValue> {
-        match InterfaceWasm::scale_print(scale_short, tonic) {
+        match self.interface.scale_print(scale_short, tonic) {
             Ok(res) => Ok(res),
             Err(e) => Err(JsValue::from_str(e.name.as_str())),
         }
     }
 
+    /// Generate chord list for a scale
+    ///
+    /// # Arguments
+    ///
+    /// * `scale_short` - // TODO for complete the documentation check out in
+    /// file in other rust project
+    /// * `tonic` - Valid note (parsed after in Note::from_str) or Catch
+    /// * `fret_position` - Fret position
     #[wasm_bindgen(catch)]
     pub fn scale_chord_list(
         &self,
@@ -124,7 +139,9 @@ impl UkuleleWasm {
         tonic: &str,
         fret_position: FretID,
     ) -> Result<String, JsValue> {
-        match InterfaceWasm::scale_chord_list(scale_short, tonic, fret_position)
+        match self
+            .interface
+            .scale_chord_list(scale_short, tonic, fret_position)
         {
             Ok(res) => Ok(res),
             Err(e) => Err(JsValue::from_str(e.name.as_str())),
